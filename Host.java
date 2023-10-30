42b861bbad159e374ad827548b618872bab343c6
noor <noorulzayn10@gmail.com>
2023-10-30 21:23:19 +0200

    added getters and setters

--- Host.java
+++ Host.java
@@ -7,4 +7,38 @@ public class Host {
     private String lastName;
     private Date registrationDate;
 
+
+    public int getHostID() {
+        return hostID;
+    }
+
+    public void setHostID(int hostID) {
+        this.hostID = hostID;
+    }
+
+    public String getFirsName() {
+        return firsName;
+    }
+
+    public void setFirsName(String firsName) {
+        this.firsName = firsName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+
 }
