0ec2961b5b5ae572c9665e0172e449663cb59b61
noor <noorulzayn10@gmail.com>
2023-10-30 21:08:51 +0200

    added function prototypes

--- BASIC.java
+++ BASIC.java
@@ -1,8 +1,25 @@
 
 
 public class BASIC {
-    public static void main(String args[]){
+    public static void main(String[] args){
+
 
 
     }
+
+
+    public void menu(){}
+    public void addUser(){}
+    public void deleteUser(int id){}
+    public void getUserDetails(int id){}
+    public void addProperty(){}
+    public void deleteProperty(int id){}
+    public void getPropertyDetails(int id){}
+    public void addBooking(int a, int b){}
+    public void getUserBooking(int id){}
+    public void listUsers(){}
+    public void listProperties(){}
+    public void exit(){}
+
+
 }
