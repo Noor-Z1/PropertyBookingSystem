901c80bdf94f1236a0206eebed6235beecbabb13
noor <noorulzayn10@gmail.com>
2023-11-08 20:16:43 +0200

    version2

--- PopulateData.java
+++ PopulateData.java
@@ -2,7 +2,10 @@ import java.util.ArrayList;
 import java.util.Date;
 
 
-
+/**
+ * @author Noor Ul Zain
+ * @version 1.0
+ */
 
 class PopulateData {
 
@@ -17,11 +20,9 @@ class PopulateData {
             p2.setHost( new Host(33, "John", "Karis", new Date("22/5/2018")) );
             p3.setHost( new Host(44, "Carolina", "Silva", new Date("17/8/2022")) );
 
-
-//        Booking b1 = new Booking(new Date("4/06/2023"), new Date("01/07/2023"), true, p1);
-//        Booking b2 = new Booking(new Date("9/08/2023"), new Date("01/09/2023"), false, p2);
-//        Booking b3 = new Booking(new Date("14/10/2023"), new Date("01/11/2023"), true, p3);
-
+            Booking b1 = new Booking(new Date("01/2/2017"), new Date("02/2/2017"), p1);
+            Booking b2 = new Booking(new Date("22/5/2018"), new Date("23/5/2018"), p2);
+            Booking b3 = new Booking(new Date("17/8/2022"), new Date("18/8/2022"), p3);
 
             // add users (custom)
             User u1 = new User(2243);
@@ -29,18 +30,18 @@ class PopulateData {
             User u3 = new User(9239);
 
 
-//        u1.setFirstName("Leena");
-//        u1.setLastName("Smith");
-//        u1.setDateOfBirth(new Date("12/7/2001"));
-//        u1.addBooking(b1);
-//        u2.setFirstName("Amber");
-//        u2.setLastName("Jade");
-//        u2.setDateOfBirth(new Date("4/2/1997"));
-//        u2.addBooking(b2);
-//        u3.setFirstName("Sarah");
-//        u3.setLastName("Williams");
-//        u3.setDateOfBirth(new Date("30/1/1999"));
-//        u3.addBooking(b3);
+            u1.setFirstName("Leena");
+            u1.setLastName("Smith");
+            u1.setDateOfBirth(new Date("12/7/2001"));
+            u1.addBooking(b1);
+            u2.setFirstName("Amber");
+            u2.setLastName("Jade");
+            u2.setDateOfBirth(new Date("4/2/1997"));
+            u2.addBooking(b2);
+            u3.setFirstName("Sarah");
+            u3.setLastName("Williams");
+            u3.setDateOfBirth(new Date("30/1/1999"));
+            u3.addBooking(b3);
 
 
             BASIC.users = new ArrayList<User>();
